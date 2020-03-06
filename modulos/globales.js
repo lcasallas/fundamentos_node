7e8ea3bbeceb9a3fd2da358cda56057edfd1a6f3
Leonardo Beltran <leonardo.casallas.beltran@gmail.com>
2020-03-05 22:05:00 -0500

    inicio de modulos

---
+++ modulos/globales.js
@@ -0,0 +1,18 @@
+// import { setImmediate } from 'timers';
+
+// console.log(setInterval);
+
+// let i = 0;
+// let intervalo = setInterval(function() {
+//   console.log('hola');
+//   if (i === 3) {
+//     clearInterval(intervalo);
+//   }
+//   i++;
+// }, 1000);
+
+// setImmediate(function() {
+//   console.log('hola');
+// });
+
+console.log(__filename);
